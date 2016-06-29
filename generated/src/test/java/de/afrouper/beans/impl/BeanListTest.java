18cf63c1839a9512dc6ba0efb1ee1f5138862e72
Christian Kemper <christian.kemper@gmx.de>
2016-06-29 21:27:22 +0200

    ListTest

--- generated/src/test/java/de/afrouper/beans/impl/BeanListTest.java
+++ generated/src/test/java/de/afrouper/beans/impl/BeanListTest.java
@@ -7,24 +7,39 @@ import de.afrouper.beans.BeanList;
 
 public class BeanListTest {
 
+	private static final int MAX = 5;
+
 	@Test
 	public void simpleList() {
+		BeanList<Person> persons = createPersonsList();
+		Assert.assertEquals(MAX, persons.size());
+
+		for (int i = 0; i < MAX; ++i) {
+			Person person = persons.get(i);
+			Assert.assertEquals(i, person.getAge().intValue());
+			Assert.assertEquals("Name_" + (i + 1), person.getName());
+		}
+	}
+
+	@Test
+	public void childLists() {
+		Person person = BeanFactory.createBean(Person.class);
+		person.setChilds(createPersonsList());
+
+		Assert.assertNotNull(person.getChilds());
+		Assert.assertEquals(MAX, person.getChilds().size());
+	}
+
+	private BeanList<Person> createPersonsList() {
 		BeanList<Person> persons = BeanFactory.createBeanList(Person.class);
 		Assert.assertNotNull(persons);
 
-		final int MAX = 5;
 		for (int i = 0; i < MAX; ++i) {
 			Person person = BeanFactory.createBean(Person.class);
 			person.setAge(i);
 			person.setName("Name_" + (i + 1));
 			persons.add(person);
 		}
-		Assert.assertEquals(MAX, persons.size());
-
-		for (int i = 0; i < MAX; ++i) {
-			Person person = persons.get(i);
-			Assert.assertEquals(i, person.getAge().intValue());
-			Assert.assertEquals("Name_" + (i+1), person.getName());
-		}
+		return persons;
 	}
 }
--- generated/src/test/java/de/afrouper/beans/impl/Person.java
+++ generated/src/test/java/de/afrouper/beans/impl/Person.java
@@ -1,6 +1,7 @@
 package de.afrouper.beans.impl;
 
 import de.afrouper.beans.Bean;
+import de.afrouper.beans.BeanList;
 
 public interface Person extends Bean {
 
@@ -17,4 +18,8 @@ public interface Person extends Bean {
 	Person getPartner();
 
 	void setPartner(Person person);
+
+	BeanList<Person> getChilds();
+
+	void setChilds(BeanList<Person> childs);
 }
